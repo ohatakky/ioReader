5c25974ac40be8122b61a603d66508d5bc5d806e
ohatakky <ohatakky@gmail.com>
2019-04-07 13:40:02 +0900

    バッファにReadする

---
+++ f/inetrface.go
@@ -0,0 +1,12 @@
+package f
+
+// 引数pは読み込んだデータを貯めるバッファ
+// type Reader interface {
+// 	Read(p []byte) (n int, err error)
+// }
+
+// goでメモリ確保するなら組み込みのmakeを使うのが良い
+// func MakeBuffer() {
+// 	buffer := make([]byte, 1024)
+// 	size, err := r.Read(buffer)
+// }
---
+++ go.mod
@@ -0,0 +1,6 @@
+module ioReader
+
+require (
+	golang.org/x/arch v0.0.0-20190312162104-788fe5ffcd8c // indirect
+	golang.org/x/crypto v0.0.0-20190404164418-38d8ce5564a5 // indirect
+)
